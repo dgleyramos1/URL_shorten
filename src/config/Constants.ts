cac7ff73db7fa1c8ce940fb3e35295f9422c2bb8
Dgley Ramos <dgleysilva@gmail.com>
2022-01-29 08:56:18 -0300

    finish

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: "mongodb+srv://dgleyramos20:Edg141512@!@url-short.jo3np.mongodb.net/url-short?retryWrites=true&w=majority"
+}
---
+++ src/controller/URLController.ts
@@ -0,0 +1,37 @@
+import { URLModel } from './../database/model/URL';
+import { config } from './../config/Constants';
+import { Request, Response } from 'express';
+import shortId from 'shortid';
+
+
+
+export class URLController{
+    public async shorten(req: Request, res: Response): Promise<void> {
+
+        const { originURL } = req.body
+        const url = await URLModel.findOne({ originURL })
+        if(url){
+            res.json(url)
+            return
+        }
+
+        const hash = shortId.generate();
+        const shortURL = `${config.API_URL}/${hash}`
+        const newUrl = await URLModel.create({ hash, shortId, shortURL })
+
+
+        res.json(newUrl)
+    }
+
+    public async redirect(req: Request, res: Response): Promise<void>{
+        const { hash } = req.params
+        const url = await URLModel.findOne({ hash })
+        if(url){
+            res.redirect(url.originURL);
+            return;
+        }
+
+        res.status(400).json({error: 'URL not found'});
+    }
+
+}
